1d3043579a29e11bea94d050c54c4792e168c358
Tim Smith <timasmith613@gmail.com>
2020-07-27 01:07:01 -0400

    commit Sprint Two Finish

--- hw2-sprint1.cpp
+++ hw2-sprint1.cpp
@@ -28,57 +28,66 @@ int main()
     {
         Array1[count] = num1[num1End] -48;
         num1End--;
+        //Debug Array1
+        //cout << Array1[count] << endl;
     }
     count = 0;
    for(count; count < num2Length; count++)
     {
         Array2[count] = num2[num2End] -48;
         num2End--;
+        //Debug Array2
+        //cout << Array2[count] << endl;
     }
-    cout << "Time to combine!" << endl;
     combineArray(Array1, num1Length, Array2, num2Length);
-   system("pause");
-   return 0;
 }
 
 //sprint two, combine arrays and export value
-void combineArray(int Array1[], int length1, int Array2[], int length2)
+void combineArray(int Array1[],int length1,int Array2[],int length2)
 {
     int fullLength = 0;
-    if (length1 > length2)
+    if (length1 >= length2)
     {
-    int fullLength = length1;
+    fullLength = length1;
     }
     else
     {
-        {
-            int fullLength = length2;
-        }
+     fullLength = length2;
     }
-    
-    int combinedArray[fullLength] = {0};
+    //Debug Length
+    /*
+    cout << "Length 1" << length1 << endl;
+    cout << "Length 2" << length2 << endl;
+    cout << "total length:" << fullLength << endl;
+    */
 
+   int combinedArray[fullLength] = {0};
     for(int count = 0; count < fullLength; count ++)
     {
-        if((Array1[count] + Array2[count]) > 10)
+        if((combinedArray[count] + Array1[count] + Array2[count]) >= 10)
         {
-            combinedArray[count] = combinedArray[count] + (Array1[count] + Array2[count] - 10);
+            combinedArray[count] = combinedArray[count] + Array1[count] + Array2[count] - 10;
             combinedArray[count+1] = 1;
+            //Debug Tens Place
+            //cout << "Tens Moved" << endl;
         }
         else
         {
             {
                 combinedArray[count] = combinedArray[count] + Array1[count] + Array2[count];
+                //Debug Typical Case
+                //cout << "value kept!" << endl;
             }
-        }
-        
-        for(int output = fullLength; output >= 0; output--)
-        {
-            if (combinedArray[output] == 0 && output == fullLength)
-            {
-                output--;
-            }
-            cout << combinedArray[output] << endl;
         }
     }
+        for(int output = 0; output < fullLength; output++)
+        {
+            cout << combinedArray[output] << ",";
+            if (combinedArray[output+1] != 0 && output == fullLength - 1)
+            {
+                cout << combinedArray[output + 1];
+            }
+        }
+    
+    
 }
